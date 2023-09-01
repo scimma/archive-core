62758c556b87a378c71f94a164cc729a8e80465c
C. Weaver <cweaver@icecube.wisc.edu>
2023-09-01 17:01:09 -0500

    Make native postgres work better on systems which restirct use of /var/run/postgres

--- tests/conftest.py
+++ tests/conftest.py
@@ -100,6 +100,7 @@ class NativePostgresServer(PostgresServer):
 			"lc_numeric": "'en_US.UTF-8'",
 			"lc_time": "'en_US.UTF-8'",
 			"default_text_search_config": "'pg_catalog.english'",
+			"unix_socket_directories": "'"+self.datadir+"'"
 		}
 		defaults.update(kwargs)
 		with open(self.datadir+"/postgresql.conf", 'w') as f:
@@ -110,7 +111,7 @@ class NativePostgresServer(PostgresServer):
 				f.write('\n')
 
 	def __init__(self, datadir: str, max_start_attempts: int = 10):
-		self.datadir = datadir
+		self.datadir = str(datadir)
 		logging.info("Initialiing postgres in", self.datadir)
 		log_path = datadir+"/logfile"
 		init_result = self.run_control("init")
